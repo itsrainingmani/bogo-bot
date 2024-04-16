b737a7c40bed6c1770baf116a7d22e5c64ccc55d
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-04-16 12:40:25 -0400

    split time and food pref into functions. auto subscribe user if they are specifying time and food pref

--- server.py
+++ server.py
@@ -15,32 +15,32 @@ load_dotenv()
 
 COMMANDS = """
 Commands:
-* `show deals`: to display available BOGO deals
-* `status`: to show your subscription status
+* `show deals` : to display available BOGO deals
+* `status`     : to show your subscription status
 * `whos hungry`: to show who is currently waiting to get paired
-* `schedule`: to set your weekly pairing schedule
+* `schedule`   : to set your weekly pairing schedule
   * ex. `schedule mon tue thu`, `schedule mon fri`
-* `subscribe`: to start getting matched with other BOGOBot users for pair lunching
+* `subscribe`  : to start getting matched with other BOGOBot users for pair lunching
 * `unsubscribe`: to stop getting matched
 """
 
 
 SHOW_DEALS_FIRST = utils.get_show_deals_message() + """
-    
+
 *Would you like to be paired?*
 * `sign me up boss` or `shaddup`
 """ #dont remove the blank line in this...
- 
+
 TIME_OPTIONS = {"a": "11 am - 12 pm", "b": "12 pm - 1 pm", "c": "1 pm - 2 pm", "d": "2 pm - EOD"}
 TIME_COMMANDS = f"""
 Please response with `time` followed by the interval letters you are free for lunch.
 {"".join([f"> {ch}. {TIME_OPTIONS[ch]}\n" for ch in "abcd"])}
 * ex. `time all`, `time d`, `time a c d`, `time b c`
-""" 
+"""
 
 FOOD_OPTIONS = {'1': "Rice bowls (burrito, shawarma, asian)", '2': "Sandwich/burger", '3': "Pizza", '4': "Fried foods (chicken, other)", '5': "Vegetarian, vegan"}
 FOOD_PREF_COMMANDS = f"""
-Since BOGO deals are changing throughout the day, we'll pair people on general food preferences. 
+Since BOGO deals are changing throughout the day, we'll pair people on general food preferences.
 Please response with `pref` followed by the food numbers you'd be willing to get with someone today.
 {"".join([f"> {i}. {FOOD_OPTIONS[str(i)]}\n" for i in range(1,6)])}
 * ex. `pref all`, `pref 1 3 4`, `pref 5`
@@ -78,27 +78,27 @@ def handle():
     ##       DEV RESPONSES          ##
         elif content == "today":
             zulip_message.daily_message_blast()
-            return {"content": 'testing...'} 
-        
+            return {"content": 'testing...'}
+
         elif content == "sign me up boss":
             data = utils.queue_user(supabase_client, sender_id)
             return {"content": TIME_COMMANDS}
-        
+
         elif content[:4] == "time":
             time_pref = content[4:].split()
-            response = utils.queue_user(supabase_client, sender_id, times=time_pref)
+            response = utils.update_time_preference(supabase_client, sender_id, times=time_pref)
             updated_times = [TIME_OPTIONS[ch] for ch in response]
-            content_response = f"Your time interval preferences were updated: {', '.join(updated_times)}\n"
+            content_response = f"Your time interval preferences were updated to: {', '.join(updated_times)}\n"
             return {"content": content_response + FOOD_PREF_COMMANDS}
 
         elif content[:4] == 'pref':
             food_pref = content[4:].split()
-            response = utils.queue_user(supabase_client, sender_id, prefs=food_pref)
+            response = utils.update_food_preference(supabase_client, sender_id, prefs=food_pref)
             updated_prefs = [FOOD_OPTIONS[i].lower() for i in response]
-            content_response = f"Your cuisine preferences were updated: {', '.join(updated_prefs)}\n"
+            content_response = f"Your cuisine preferences were updated to: {', '.join(updated_prefs)}\n"
             return {"content": content_response + 'WE CHECKING FOR A MATCH OK'}
 
-        
+
         elif content == "whos hungry":
             pass
 
@@ -109,7 +109,7 @@ def handle():
 
         elif content == "show deals first":
             return {"content": SHOW_DEALS_FIRST}
-        
+
         elif content == "all subs":
             all_data = utils.get_subscribed_users(supabase_client)
             all_users = []
